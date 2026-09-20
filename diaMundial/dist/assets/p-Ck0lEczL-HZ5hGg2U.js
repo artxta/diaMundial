90f11289ecd52b5c27e2dc35a3f457a2d3ecd08f
Jose Alberto Torresano <joseichiban91@gmail.com>
2026-09-20 16:50:19 +0200

    Reorganizar proyectos: app-android/, app-web/, limpiar node_modules y dia_mundial_old

--- diaMundial/dist/assets/p-Ck0lEczL-HZ5hGg2U.js
+++
@@ -1,3 +0,0 @@
-import{L as s,M as r,N as a,P as i,O as c}from"./ionic-BiQLqjFv.js";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const m=()=>{const e=window;e.addEventListener("statusTap",(()=>{s((()=>{const n=document.elementFromPoint(e.innerWidth/2,e.innerHeight/2);if(!n)return;const t=r(n);t&&new Promise((o=>a(t,o))).then((()=>{i((async()=>{t.style.setProperty("--overflow","hidden"),await c(t,300),t.style.removeProperty("--overflow")}))}))}))}))};export{m as startStatusTap};
